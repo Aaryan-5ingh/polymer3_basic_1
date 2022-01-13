11bea8714aa81fc57678c87f2e6f03f02e18966d
Aaryan-5ingh <97450527+Aaryan-5ingh@users.noreply.github.com>
2022-01-14 02:24:50 +0530

    Add files via upload
    grid

--- icon-toggle.js
+++ icon-toggle.js
@@ -5,16 +5,10 @@ class IconToggle extends PolymerElement {
   static get template() {
     return html`
       <style>
-        /* shadow DOM styles go here */
-        span {
-          color: blue;
-        }
-        :host {
-          display: inline-block;
-        }
-      </style>
-  
-      <!-- shadow DOM goes here -->
+	:host{display:grid; grid-template-columns:repeat(3,1fr); grid-gap:4px 8px;}
+       </style>
+	<p>The iron-icon element renders the icon specified by its icon attribute.Here, iron-icon is hard-coded to use an icon named polymer.</p>
+	<p>Ludwig van Beethoven said, <q>Music should strike fire from the heart of a man, and bring tears from the eyes of a woman. Don't only practice your art, but force your way into its secrets. Art deserves that. </q></p>
       <span>hi!<hr /> <iron-icon icon="polymer"></iron-icon></span>
     `;
   }
